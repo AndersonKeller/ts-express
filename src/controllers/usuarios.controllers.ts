76fe275e17162f9680e59487b9eb31fff7ef6d64
AndersonKeller <andersonkellervendas@hotmail.com>
2025-05-28 21:16:50 -0300

    validações de dados

--- src/controllers/usuarios.controllers.ts
+++ src/controllers/usuarios.controllers.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from "express"
 import { createUserService } from "../services/createUser.service"
 import { getAllUsersService } from "../services/getAllUsers.service"
+import { ReturnUser } from "../schemas/usuarios.schemas"
 
 
-export const createUserController = async(req:any,res:any):Promise<any> =>{
+export const createUserController = async(req:Request,res:Response):Promise<any> =>{
     const userData = req.body
-    const user = await createUserService(userData)
+    const user:ReturnUser = await createUserService(userData)
     return  res.status(201).json(user)
     
 }
-export const getAllUsersController=async(req:any,res:any):Promise<any>=>{
+export const getAllUsersController=async(req:Request,res:Response):Promise<any>=>{
     const users = await getAllUsersService()
     return res.status(200).json(users)
 }
