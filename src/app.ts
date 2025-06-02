5f5543a60eaa4bcd2a934dd78d9e66f82e250dd7
AndersonKeller <andersonkellervendas@hotmail.com>
2025-06-02 19:34:44 -0300

    add: bcrypt e post entitie

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import 'express-async-errors'
 import express, { Application } from "express"
 import { usuariosRoutes } from "./routes/usuarios.routes"
 import { handleErrors } from "./errors"
+import { postsRoutes } from './routes/posts.routes'
 const app:Application = express()
 
 app.use(express.json())
 app.use("/usuarios",usuariosRoutes)
-
+app.use("/posts",postsRoutes)
 
 app.use(handleErrors)
 export default app
